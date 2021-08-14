78732add0b0d9449833657f8063ef71c820bd9d1
cf-whyte <cfwhytedev@gmail.com>
2021-08-14 13:04:05 +1200

    src components, evidence table and table style

--- client/src/components/EvidenceTable.js
+++ client/src/components/EvidenceTable.js
@@ -1,7 +1,7 @@
 import React, {useMemo} from 'react';
 import articles from '../dummydata/articles';
 import {useTable, useSortBy, usePagination} from 'react-table';
-
+import styles from './TableStyle';
 const Table = ({columns, data}) => {
 	const {
 		getTableProps,
@@ -9,8 +9,8 @@ const Table = ({columns, data}) => {
 		headerGroups,
 		prepareRow,
 
-		page, 
-        // Instead of using 'rows', we'll use page,
+		page,
+		// Instead of using 'rows', we'll use page,
 		// which has only the rows for the active page
 		// The rest of these things are super handy, too ;)
 		canPreviousPage,
@@ -43,7 +43,11 @@ const Table = ({columns, data}) => {
 							{headerGroup.headers.map((column) => (
 								// Add the sorting props to control sorting. For this example
 								// we can add them into the header props
-								<th {...column.getHeaderProps(column.getSortByToggleProps())}>
+								<th
+									{...column.getHeaderProps(
+										column.getSortByToggleProps()
+									)}
+								>
 									{column.render('Header')}
 									{/* Add a sort direction indicator */}
 									<span>
@@ -65,7 +69,9 @@ const Table = ({columns, data}) => {
 							<tr {...row.getRowProps()}>
 								{row.cells.map((cell) => {
 									return (
-										<td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+										<td {...cell.getCellProps()}>
+											{cell.render('Cell')}
+										</td>
 									);
 								})}
 							</tr>
@@ -75,17 +81,23 @@ const Table = ({columns, data}) => {
 			</table>
 
 			{/* Pagination */}
-			<div className="pagination">
+			<styles>
 				<button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
 					{'<<'}
 				</button>{' '}
-				<button onClick={() => previousPage()} disabled={!canPreviousPage}>
+				<button
+					onClick={() => previousPage()}
+					disabled={!canPreviousPage}
+				>
 					{'<'}
 				</button>{' '}
 				<button onClick={() => nextPage()} disabled={!canNextPage}>
 					{'>'}
 				</button>{' '}
-				<button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+				<button
+					onClick={() => gotoPage(pageCount - 1)}
+					disabled={!canNextPage}
+				>
 					{'>>'}
 				</button>{' '}
 				<span>
@@ -97,10 +109,12 @@ const Table = ({columns, data}) => {
 				<span>
 					| Go to page:{' '}
 					<input
-						type="number"
+						type='number'
 						defaultValue={pageIndex + 1}
 						onChange={(e) => {
-							const page = e.target.value ? Number(e.target.value) - 1 : 0;
+							const page = e.target.value
+								? Number(e.target.value) - 1
+								: 0;
 							gotoPage(page);
 						}}
 						style={{width: '100px'}}
@@ -118,7 +132,7 @@ const Table = ({columns, data}) => {
 						</option>
 					))}
 				</select>
-			</div>
+			</styles>
 		</>
 	);
 };
